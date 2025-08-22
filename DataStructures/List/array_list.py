206eb281576adbb8065b6d115fb3608437bc7aa9
Juliana-_Rod-1234 <js.rodriguezm1234@uniandes.edu.co>
2025-08-22 07:52:43 -0500

    Array_list changes

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -4,3 +4,19 @@ def new_list():
         "size": 0,
         }
     return newlist
+
+def get_element(my_list, index):
+    return my_list["elements"][index]
+
+def is_present(my_list, element, cmp_function):
+    size = my_list["size"]
+    if size > 0:
+        keyexit = False
+        for keypos in range(0, size):
+            info = my_list["elements"][keypos]
+            if cmp_function(element, info) == 0:
+                keyexit = True
+                break
+        if keyexit:
+            return keypos
+    return -1
